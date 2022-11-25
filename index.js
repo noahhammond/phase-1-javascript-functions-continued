55e918c72d792b84704595b97f305d758c68b6f4
Noah Hammond <business.noah@hotmail.com>
2022-11-25 13:09:55 -0700

    commit tree 82416380f4c936f648faf02c9f7693060d7a53ad

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork(work = 'go to the office') {
+    return (`This Monday, I will ${work}.`);
+}
+mondayWork();
+
+function wrapAdjective (str= '*') {
+    return function(name = 'special') {
+        return `You are ${str}${name}${str}!`
+    }
+}
